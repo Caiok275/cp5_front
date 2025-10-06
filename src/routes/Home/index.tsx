a9e9b929b50f39a1e7356f7272e4067ac65666e1
Caio Tayra <caio.kenzo.t@gmail.com>
2025-10-05 22:11:46 -0300

    Update index.tsx

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <br />
         <img src={LogoHC} alt="Imagem da página home" className="flex flex-col size-2/9"/>
         <br />
-        <h1 className="text-5xl text-blue-600 font-bold">Bem vindo ao cadastro de Funcionário FMUSP</h1>
+        <h1 className="text-5xl text-blue-600 font-bold">Bem vindo ao cadastro de Funcionários FMUSP</h1>
         <br />
         <p>Veja as infomrações de todos os funcionários do hospital, caso não esteja cadastrado basta preencher o formulario. Clique no botão a baixo para ver a lista:</p>
         <br />
@@ -19,3 +19,4 @@ export default function Home() {
     </div>
 
   )}
+
